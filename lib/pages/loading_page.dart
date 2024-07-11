717917c2d78a4909e76da310710f1298bd2e5c58
laubrener <79546818+laubrener@users.noreply.github.com>
2024-07-11 19:27:17 -0300

    loadingPage

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:sephora_app/widgets/background.dart';
 
 class LoadingPage extends StatelessWidget {
   const LoadingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LoadingPage'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: const Image(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/fondo_labial.png'),
+            ),
+          ),
+          Center(
+            child: CircularProgressIndicator(
+              color: Color(int.parse('0xffD93930')),
+            ),
+          )
+        ],
       ),
     );
   }
